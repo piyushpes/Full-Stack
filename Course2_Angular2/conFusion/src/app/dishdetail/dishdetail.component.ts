435b1a23d478778054e9bf7a77e2a15de17d28de
Piyush Jain <piyushpes@gmail.com>
2018-02-28 01:58:10 +0530

    Angular 2 Assignment 3

--- Course2_Angular2/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Course2_Angular2/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,8 @@ import {Params, ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
 import {DishService} from '../services/dish.service';
 import 'rxjs/add/operator/switchMap';
+import {FormBuilder,FormGroup,Validators, RequiredValidator} from '@angular/forms';
+import {Comment} from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
@@ -18,7 +20,23 @@ export class DishdetailComponent implements OnInit {
   dishids:number[];
   prev:number;
   next:number;
-  constructor(private dishservice:DishService, private location:Location,private route:ActivatedRoute) { }
+  comment:Comment={rating:5,comment:'',author:'',date:''};
+
+  userCommentFormErrors={
+    'author':'',
+    'comment':''
+  };
+
+  errorMessages={
+    'author':{'required':"Name is required",
+              'minlength':"Name should be of minimum 2 characters"},
+    'comment':{'required':"Comment is required"}
+  };
+
+  userCommentForm:FormGroup;
+  constructor(private dishservice:DishService, private location:Location,private route:ActivatedRoute,private fb:FormBuilder) { 
+    this.createForm();
+  }
 
   ngOnInit() {
     //let id=+this.route.snapshot.params['id'];
@@ -38,6 +56,56 @@ export class DishdetailComponent implements OnInit {
 
   goBack():void{
     this.location.back();
+  }
+
+  createForm(){
+    this.userCommentForm=this.fb.group({
+      'author':['',[Validators.required,Validators.minLength(2)]],
+      'comment':['',[Validators.required]],
+      'rating': 5,
+      'date':''
+    });
+    this.userCommentForm.valueChanges.subscribe(()=>this.OnValueChanged())
+  }
+  onSubmit(){
+    
+
+    this.comment.date=(new Date).toISOString();
+
+    this.dish.comments.push(this.comment);
+    this.comment={rating:5,comment:'',author:'',date:''};
+    this.userCommentForm.reset({
+      'author':'',
+      'comment':'',
+      'rating': 5,
+      'date':'' 
+    });
+  }
+  OnValueChanged(){
+    
+    if(!this.userCommentForm){
+      return;
+    }
+    //console.log(this.userCommentForm.get('rating').value);
+    for(let prop in this.comment){
+      if(this.userCommentForm.get(prop))
+      this.comment[prop]=this.userCommentForm.get(prop).value
+    }
+    console.log(this.comment); 
+    let errorObj;
+    
+    for( let field in this.userCommentFormErrors){
+      this.userCommentFormErrors[field]="";
+      if(this.userCommentForm && this.userCommentForm.dirty && this.userCommentForm.invalid)
+      {
+      errorObj=this.userCommentForm.get(field).errors;
+      for(let err in errorObj){
+        this.userCommentFormErrors[field]+= this.errorMessages[field][err] + ' ';
+      }
+    }
+    }
+
+
   }
 
 }
