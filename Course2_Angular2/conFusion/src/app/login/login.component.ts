aa51fe3d0ed46da2534e7c16c1b37362cf0fb409
Piyush Jain <piyushpes@gmail.com>
2018-03-16 23:10:52 +0530

    Angular 2 Webpack Bundle and deployment

--- Course2_Angular2/conFusion/src/app/login/login.component.ts
+++ Course2_Angular2/conFusion/src/app/login/login.component.ts
@@ -7,7 +7,7 @@ import {MdDialog,MdDialogRef} from '@angular/material'
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  user={remember:false};
+  user={remember:false,username:'',password:''};
   constructor(private dialogRef:MdDialogRef<LoginComponent>) { }
 
   ngOnInit() {
